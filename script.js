292a08f666f72ff50a52f4aa3b69a7e6b7b4f6fa
joanusar <87675358+joanusar@users.noreply.github.com>
2022-11-26 17:59:10 -0500

    Punto 1.1
    Punto 1 Javascript prueba procesalab Jose Usuga

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-//1
-var nombre = document.getElementById('nombre');
-var subjet = document.getElementById('subjet');
-var email = document.getElementById('email');
-var phone = document.getElementById('phone');
-var error = document.getElementById('error');
-
-function enviarformulario(){
-  console.log("Enviando formulario...");
-
-  var mensajesError = [];
-  if(nombre.value === null || nombre.value === ''){
-    mensajesError.push('campo nombre vacio')
-  }
-  if(subjet.value === null || subjet.value === ''){
-    mensajesError.push('Campo subject vacio')
-  }
-  if(email.value === null || email.value === ''){
-    mensajesError.push('Campo email vacio')
-  }
-  if(phone.value === null || phone.value === ''){
-    mensajesError.push('Campo phone vacio')
-  }
- error.innerHTML = mensajesError.join(', ');
-  return false;
-}
+//En consolsa
+document.querySelector("#navbarTogglerDemo02 > :nth-child(1) > :nth-child(1)").textContent.trim()
+document.querySelector("#navbarTogglerDemo02 > :nth-child(1) > :nth-child(2)").textContent.trim()
+document.querySelector("#navbarTogglerDemo02 > :nth-child(1) > :nth-child(3)").textContent.trim()
+document.querySelector("#navbarTogglerDemo02 > :nth-child(1) > :nth-child(4)").textContent.trim()
+document.querySelector("#navbarTogglerDemo02 > :nth-child(2) > :nth-child(1)").textContent.trim()
+document.querySelector("#navbarTogglerDemo02 > :nth-child(2) > :nth-child(2)").textContent.trim()
