2a3cff985a9295d6f8b03bd9e1aa4e8099e25df4
joanusar <87675358+joanusar@users.noreply.github.com>
2022-11-26 18:04:22 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-//En consolsa
-document.querySelector("#navbarTogglerDemo02 > :nth-child(1) > :nth-child(1)").textContent.trim()
-document.querySelector("#navbarTogglerDemo02 > :nth-child(1) > :nth-child(2)").textContent.trim()
-document.querySelector("#navbarTogglerDemo02 > :nth-child(1) > :nth-child(3)").textContent.trim()
-document.querySelector("#navbarTogglerDemo02 > :nth-child(1) > :nth-child(4)").textContent.trim()
-document.querySelector("#navbarTogglerDemo02 > :nth-child(2) > :nth-child(1)").textContent.trim()
-document.querySelector("#navbarTogglerDemo02 > :nth-child(2) > :nth-child(2)").textContent.trim()
